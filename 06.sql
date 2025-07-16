ffed709bc1986a72ec8460e1c038de8d9c183fd2
Maria Luiza Prata <marialuizabprata@hotmail.com>
2025-07-16 16:52:18 -0300

    Update 06.sql

--- 06.sql
+++ 06.sql
@@ -1,1 +1,17 @@
 //colocar restrições nas tabelas já utilizadas de base 
+
+COLOCA SEQUENCIA DE CODIGOS
+  
+create sequence sequencia_cod;
+insert into funcionario(cod)
+values (nextval('sequencia_cod'));
+select NEXTVAL('sequencia_cod');
+
+INSERT INTO funcionario(cod, nome, salario)
+VALUES (nextval('sequencia_cod'), 'MALY', 3.000); //
+
+--------------------------------------
+TRANSFORMA OS EMAILS EM ÚNICOS 
+  
+alter table aluno
+add constraint email_unico unique (email);
